885f5cf3d682a463ceea7ee84e5ca7cee806ccbf
yang_lin_qin <yanglinqin@zuzuche.com>
2019-10-12 16:30:12 +0800

    修正重复定义问题

--- src/ConfigPure.php
+++ src/ConfigPure.php
@@ -23,6 +23,9 @@ include_once 'Regions/EndpointConfig.php';
 include_once 'Regions/LocationService.php';
 
 //config http proxy
+/*define('ENABLE_HTTP_PROXY', false);
+define('HTTP_PROXY_IP', '127.0.0.1');
+define('HTTP_PROXY_PORT', '8888');*/
 if (!defined('ENABLE_HTTP_PROXY')) {
     define('ENABLE_HTTP_PROXY', false);
 }
